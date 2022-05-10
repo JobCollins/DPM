9f8f448e1585dd59515288f2225842b876063435
JobCollins <dulo.collins@gmail.com>
2022-05-10 14:42:46 +0300

    Granularity, Distribution, and Modeling
    Trends, Enrichment and Visualization

--- sql/reviews_f.sql
+++ sql/reviews_f.sql
@@ -1,3 +1,7 @@
 SELECT country, STDDEV(review) AS STD 
 FROM reviews_f WHERE product_id=3 
 GROUP BY country ORDER BY STD DESC
+
+select country, avg(review) as average_rating 
+from reviews_f group by country 
+order by average_rating desc
